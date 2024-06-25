26ef7b366a1530b9afa25f1110a512ede7a8b2e5
Santhosh <santhoshh2197@gmail.com>
2024-06-25 19:28:08 +0530

    items page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { OrderComponent } from './order/order.component';
 import { AddOrderComponentComponent } from './add-order-component/add-order-component.component';
 import { EditOrderComponentComponent } from './edit-order-component/edit-order-component.component';
+import { ItemComponent } from './item/item.component';
 
 export const routes: Routes = [
     { path: '', component: OrderComponent },
     { path: 'neworder', component: AddOrderComponentComponent },
     { path: 'editorder/:id', component: EditOrderComponentComponent },
+    { path: 'items', component: ItemComponent },
 ];
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+import { SidebarComponent } from '../sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-item',
+  standalone: true,
+  imports: [SidebarComponent,CommonModule,MatButtonModule,
+    MatIconModule],
+  templateUrl: './item.component.html',
+  styleUrl: './item.component.css'
+})
+export class ItemComponent {
+  items: Items [] = [
+    {
+    itemName:"Mouse",
+    category:"Electronics",
+    price:450,
+    image:"Mouse img"
+  },{
+    itemName:"Mouse",
+    category:"Electronics",
+    price:450,
+    image:"Mouse img"
+
+  }];
+  constructor(private http:HttpClient,private router: Router) {
+
+  }
+
+  editItem(){
+
+  }
+
+  deleteItem(){
+
+  }
+}
+
+class Items {
+  itemName! : string
+  category!: string;
+  price!: number;
+  image!: string;
+}
